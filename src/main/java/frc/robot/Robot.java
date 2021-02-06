952975dac3bd4ac91db094a993c33d6ac4d3177b
TheMisterWolf <vezvez1234@gmail.com>
2021-02-05 20:57:29 -0800

    Test from home

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,6 +15,10 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * project.
  */
 public class Robot extends TimedRobot {
+
+
+  // Some important variables
+
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
