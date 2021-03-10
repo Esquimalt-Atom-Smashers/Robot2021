1fd29238e0830537d789df1c8df7a0aa4a9468f6
EsqAtomSmashersProgramers <32209970+Sarkhan-the-man@users.noreply.github.com>
2021-03-10 11:09:51 -0800

    Auto Drive Mode Added Mar10/2021

--- src/main/java/frc/robot/MovementComponent.java
+++ src/main/java/frc/robot/MovementComponent.java
@@ -1,6 +1,11 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+
+import java.security.KeyStore.TrustedCertificateEntry;
+
+import javax.swing.plaf.TreeUI;
+
 import edu.wpi.first.wpilibj.AnalogInput;
 
 
@@ -12,6 +17,8 @@ public class MovementComponent extends ComponentBase {
     final int distanceCM = 500; // Max distance ultrasonic is suggested for in CM
     final int maxUltraVal = 4095; // Value of ultrasonic analog read when using 5V (Probably 4095)
     double distConst = maxUltraVal/distanceCM;
+
+    boolean autoDrive = false;
 
     AnalogInput ultrasonic = new AnalogInput(0); // Analog port number
 
@@ -25,15 +32,54 @@ public class MovementComponent extends ComponentBase {
         double ultraVal = ultrasonic.getValue(); // Gets distance val from ultrasonic between 0-4095 (0V-5V)
         double currentDistance = ultraVal / distConst; // Gives dist in CM
 
-        double moveY = 0;
-        System.out.println("CD: " + currentDistance + " UltraVal: " + ultraVal);
-        if(currentDistance<=100 && stick.getRawButton(3)){
-            moveY = -0.5;
-        } else if (currentDistance > 100) {
-            moveY = -stick.getY();
+        
+        if (stick.getRawButton(4)) { //Autonomous mode switcher, Button 4 is Y
+            if (autoDrive == false)
+                autoDrive = true;
+            else autoDrive = false;
+        }
+
+        double moveY = 0; // Fwd/bkwd movement
+        double moveZ = 0; // rotate movement
+        System.out.println("CD: " + currentDistance + " UltraVal: " + ultraVal + " Stick Y:" + stick.getY());
+        // if(currentDistance<=100 && stick.getRawButton(3)){
+        //     moveY = -0.5; // Backup half speed
+        // } else if (currentDistance > 100) {
+        //     moveY = -stick.getY();
+        // }
+        
+        //Regular Driving Mode
+        if (autoDrive == false) {
+            if (currentDistance >= 200) {
+                moveY = -stick.getY();
+            }
+            else if (currentDistance < 50) {
+                moveY = 0;
+                if (stick.getRawButton(3)) {
+                    moveY = -0.5;
+                }
+            }
+            else if (currentDistance < 200) {
+                moveY = -stick.getY() / (currentDistance / 50);
+            }
+            robot.move(moveY, ALTERNATE_ROTATE ? stick.getZ() : stick.getY());
+        }
+
+        // Autonomous mode with fail safes
+        if (autoDrive == true) {
+            if (stick.getRawButton(3))
+                moveY = 0;
+            else if (stick.getZ() != 0)
+                moveZ = stick.getZ();
+            else if (currentDistance >= 75)
+                moveY = 0.5;
+            else if (currentDistance < 75)
+                moveZ = 0.5;
+            robot.move(moveY, ALTERNATE_ROTATE ? moveZ : stick.getY());
         }
         
-        robot.move(moveY, ALTERNATE_ROTATE ? stick.getZ() : stick.getY());
+            // robot.move(moveY, ALTERNATE_ROTATE ? stick.getZ() : stick.getY());
+            // robot.move(moveY, ALTERNATE_ROTATE ? moveZ : stick.getY());
     }
     
 }
