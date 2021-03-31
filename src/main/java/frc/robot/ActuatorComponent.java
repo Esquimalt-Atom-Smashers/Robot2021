592cbe9371152c32bc4c64afca14077da7f698bc
EsqAtomSmashersProgramers <32209970+Sarkhan-the-man@users.noreply.github.com>
2021-03-31 16:59:48 -0700

    kyle is paranoid

--- src/main/java/frc/robot/ActuatorComponent.java
+++ src/main/java/frc/robot/ActuatorComponent.java
@@ -6,7 +6,7 @@ public class ActuatorComponent extends ComponentBase {
 
     private static final double MAX_ANGLE = 145;
     private static final double MIN_ANGLE = 30;
-    private static final double MOVE_AMOUNT = 0.25;
+    private static final double MOVE_AMOUNT = 0.2;
     private static final int    DELAY = 6;
 
     public ActuatorComponent(Robot robot) {
